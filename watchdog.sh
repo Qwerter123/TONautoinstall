b3af0bd7444954ce46a29aad84a7818bf0569092
Qwerter123 <61710286+Qwerter123@users.noreply.github.com>
2021-11-21 00:16:35 +0300

    Create watchdog.sh

---
+++ watchdog.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+TIME_RESTART_LOOP_=360
+
+while :
+do
+<-->echo "starting miners"
+<-->./start_mining.sh 4 && echo "miner started"
+<-->sleep $TIME_RESTART_LOOP_
+<-->echo "stopping miner"
+<-->./stop_mining.sh&&echo "miner stopped"
+done
