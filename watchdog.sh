92e4f88abccb5561c640c6678f4d9aee1a66036c
Qwerter123 <61710286+Qwerter123@users.noreply.github.com>
2021-11-21 01:41:46 +0300

    Update watchdog.sh

--- watchdog.sh
+++ watchdog.sh
@@ -4,9 +4,9 @@ TIME_RESTART_LOOP_=360
 
 while :
 do
-<-->echo "starting miners"
-<-->./start_mining.sh 4 && echo "miner started"
-<-->sleep $TIME_RESTART_LOOP_
-<-->echo "stopping miner"
-<-->./stop_mining.sh&&echo "miner stopped"
+echo "starting miners"
+./start_mining.sh 4 && echo "miner started"
+sleep $TIME_RESTART_LOOP_
+echo "stopping miner"
+./stop_mining.sh&&echo "miner stopped"
 done
