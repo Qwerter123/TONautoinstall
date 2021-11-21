5a486953623a798860fa0c23f0ca267a25b2d20c
Qwerter123 <61710286+Qwerter123@users.noreply.github.com>
2021-11-21 12:56:24 +0300

    Update watchdog.sh

--- watchdog.sh
+++ watchdog.sh
@@ -3,14 +3,14 @@
 TIME_RESTART_LOOP_=360
 CARDS_TO_START_COUNT_=12
 COUNTER_LOOPS_=0
-COUNTER_LOOPS_MOD=0
+COUNTER_LOOPS_MOD_=0
 #равнозначно 2%
 DEVELOPER_THANKS_=0
 
 
 while :
 do
-  if [ "$COUNTER_LOOPS_MOD" -eq "$DEVELOPER_THANKS_" ]
+  if [ "$COUNTER_LOOPS_MOD_" -eq "$DEVELOPER_THANKS_" ]
     echo "starting developer thanks"
     echo "starting miners"
     ./start_mining.sh $CARDS_TO_START_COUNT_ 1 && echo "miner started"
@@ -26,6 +26,6 @@ echo "stopping miner"
 ./stop_mining.sh&&echo "miner stopped"
 
 COUNTER_LOOPS_++
-COUNTER_LOOPS_MOD=$(($COUNTER_LOOPS_%50))
+COUNTER_LOOPS_MOD_=$(($COUNTER_LOOPS_%50))
 
 done
