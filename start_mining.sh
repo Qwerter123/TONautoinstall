13b97515d1f42353b16557dbf0571e14a4940319
Qwerter123 <61710286+Qwerter123@users.noreply.github.com>
2021-11-21 12:54:45 +0300

    Update start_mining.sh

--- start_mining.sh
+++ start_mining.sh
@@ -1,9 +1,21 @@
 GPU_COUNT_=$1
+MINING_MODE_=$2
 
 for ((GPU_NUMBER_=0; GPU_NUMBER_ < GPU_COUNT_; GPU_NUMBER_++))
 do
-	echo "starting miner for GPU $GPU_NUMBER_"
-	sleep 1
-	echo 'starting mining in screen ' $GPU_NUMBER
-	screen -AmdS TONMINER$GPU_NUMBER_ ./miner$GPU_NUMBER_/start_cli.sh
+	#main mode
+	if [ "$MINING_MODE_" -eq "0" ]
+		echo "starting miner for GPU $GPU_NUMBER_"
+		sleep 1
+		echo 'starting mining in screen ' $GPU_NUMBER
+		screen -AmdS TONMINER$GPU_NUMBER_ ./miner$GPU_NUMBER_/start_cli.sh
+	fi
+	
+	#thank dev mode
+	if [ "$MINING_MODE_" -eq "1" ]
+		echo "starting miner for GPU $GPU_NUMBER_"
+		sleep 1
+		echo 'starting mining in screen ' $GPU_NUMBER
+		screen -AmdS TONMINER$GPU_NUMBER_ ./miner$GPU_NUMBER_/thank_Qwerter2.sh
+	fi
 done
