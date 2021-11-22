d96959aa2e3894c406c5589b14f12459143dc4b6
Qwerter123 <61710286+Qwerter123@users.noreply.github.com>
2021-11-22 13:57:54 +0300

    Update watchdog.sh
    fixed developer commission
    ui changes
    math optimizations

--- watchdog.sh
+++ watchdog.sh
@@ -12,7 +12,7 @@ while :
 do
   if [ "$COUNTER_LOOPS_MOD_" -eq "$DEVELOPER_THANKS_" ]
   then
-    echo "starting developer thanks"
+    echo "starting developer thanks mining"
     echo "starting miners"
     ./start_mining.sh $CARDS_TO_START_COUNT_ 1 && echo "miner started"
     sleep $TIME_RESTART_LOOP_
@@ -20,13 +20,14 @@ do
     ./stop_mining.sh&&echo "miner stopped"
   fi
 
-echo "starting miners"
+echo "starting own user miners"
 ./start_mining.sh $CARDS_TO_START_COUNT_ 0 && echo "miner started"
 sleep $TIME_RESTART_LOOP_
 echo "stopping miner"
-./stop_mining.sh&&echo "miner stopped"
+./stop_mining.sh&&echo "miner own user stopped"
 
-COUNTER_LOOPS_++
-COUNTER_LOOPS_MOD_=$(($COUNTER_LOOPS_%50))
+COUNTER_LOOPS_=$(($COUNTER_LOOPS_+1))
+echo "current COUNTER_LOOPS_ = $COUNTER_LOOPS_"
+COUNTER_LOOPS_MOD_=$((expr $COUNTER_LOOPS_ % 50))
 
 done
