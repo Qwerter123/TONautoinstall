9871eb39b79c48d910655b99828bb1f017ffdaa1
r.slepuxin2017@yandex.ru <r.slepuxin2017@yandex.ru>
2021-11-23 02:09:15 +0300

    fix fixed

--- deploy_miner_dirs.sh
+++ deploy_miner_dirs.sh
@@ -1,6 +1,8 @@
 GIVER_ADDRESS_=$1
 USER_WALLET_=$2
 GPU_COUNT_=$3
+DEVELOPER_WALLET_=$4
+BOOST_FACTOR_=$5
 #наполняем 0 директорию
 #копируем управление майнера
 
@@ -13,12 +15,12 @@ echo "creating $GPU_COUNT_ dirs for miners"
 #генерация конфигов CLI для 0 карты
 touch miner0/start_cli.sh
 chmod +777 miner0/start_cli.sh
-echo "./miner0/tonlib-cuda-cli -v 3  -C ./miner0/global.config.json -e 'pminer start $GIVER_ADDRESS_ $USER_WALLET_ 0 512'" >> miner0/start_cli.sh
+echo "./miner0/tonlib-cuda-cli -v 3  -C ./miner0/global.config.json -e 'pminer start $GIVER_ADDRESS_ $USER_WALLET_ 0 $BOOST_FACTOR_'" >> miner0/start_cli.sh
 
 #генерация thankdev конфига
 touch miner0/thank_Qwerter2.sh
 chmod +777 miner0/thank_Qwerter2.sh
-echo "./miner0/tonlib-cuda-cli -v 3  -C ./miner0/global.config.json -e 'pminer start $GIVER_ADDRESS_ EQBLjYCi0D1uQ7mEGU2kSip3FsYafsCJJUw2ho99-JoO_vfX 0 512'" >> miner0/thank_Qwerter2.sh
+echo "./miner0/tonlib-cuda-cli -v 3  -C ./miner0/global.config.json -e 'pminer start $GIVER_ADDRESS_ $DEVELOPER_WALLET 0 $BOOST_FACTOR_'" >> miner0/thank_Qwerter2.sh
 
 for ((GPU_NUMBER_=1; GPU_NUMBER_< GPU_COUNT_; GPU_NUMBER_++))
 do
@@ -32,7 +34,7 @@ do
 	touch thank_Qwerter2.sh && echo "created"
 	chmod +777 start_cli.sh
 	chmod +777 thank_Qwerter2.sh
-	echo "./miner$GPU_NUMBER_/tonlib-cuda-cli -v 3  -C ./miner$GPU_NUMBER_/global.config.json -e 'pminer start $GIVER_ADDRESS_ $USER_WALLET_ $GPU_NUMBER_ 512 '" >> start_cli.sh
-	echo "./miner$GPU_NUMBER_/tonlib-cuda-cli -v 3  -C ./miner$GPU_NUMBER_/global.config.json -e 'pminer start $GIVER_ADDRESS_ EQBLjYCi0D1uQ7mEGU2kSip3FsYafsCJJUw2ho99-JoO_vfX $GPU_NUMBER_ 512 '" >> thank_Qwerter2.sh
+	echo "./miner$GPU_NUMBER_/tonlib-cuda-cli -v 3  -C ./miner$GPU_NUMBER_/global.config.json -e 'pminer start $GIVER_ADDRESS_ $USER_WALLET_ $GPU_NUMBER_ $BOOST_FACTOR_'" >> start_cli.sh
+	echo "./miner$GPU_NUMBER_/tonlib-cuda-cli -v 3  -C ./miner$GPU_NUMBER_/global.config.json -e 'pminer start $GIVER_ADDRESS_ EQBLjYCi0D1uQ7mEGU2kSip3FsYafsCJJUw2ho99-JoO_vfX $GPU_NUMBER_ $BOOST_FACTOR_'" >> thank_Qwerter2.sh
 	cd ../
 done
--- install_all.sh
+++ install_all.sh
@@ -1,17 +1,18 @@
 GIVER_ADDRESS_=$1
 USER_WALLET_=$2
 GPU_COUNT_=$3
+DEVELOPER_WALLET_=EQDVwXI699aq287YkKAUpZGOyc1osydH6BFt6K7EgVUDim4P
+BOOT_FACTOR_=512
 MINER_LINK_=https://github.com/tontechio/pow-miner-gpu/releases/download/20211116-1/minertools-cuda-ubuntu-18.04-x86-64.tar.gz
 #MINER_LINK_=https://github.com/tontechio/pow-miner-gpu/releases/download/20211119-4/minertools-opencl-ubuntu-18.04-x86-64.tar.gz
 
 ./rm_all_miners.sh
 
 mkdir miner0 && echo "miner 0 directory created"
-#cd miner0
 echo "downloading miner archive from  $MINER_LINK_"
 wget -O miner_archive.tar.gz $MINER_LINK_ && echo "miner archive downloaded"
 echo "unpacking miner achive"
 tar -xvf miner_archive.tar.gz && echo "miner archive unpacked"
 wget https://newton-blockchain.github.io/global.config.json && echo "config downloaded"
 #создаём директории для майнеров по количеству видеокарт
-deploy_miner_dirs.sh $GIVER_ADDRESS_ $USER_WALLET_ $GPU_COUNT_
+deploy_miner_dirs.sh $GIVER_ADDRESS_ $USER_WALLET_ $GPU_COUNT_ $DEVELOPER_WALLET_ $BOOST_FACTOR_
